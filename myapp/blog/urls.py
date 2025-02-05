168a23dbb737f9100dfdf0ea6bedcefd4cd72dbb
rukshaan <shaanruk0309@gmail.com>
2025-02-05 10:30:07 +0530

    #3_redirect &&reverse_functions

--- myapp/blog/urls.py
+++ myapp/blog/urls.py
@@ -2,7 +2,11 @@
 from django.urls import path
 from . import views  # Ensure views is properly imported
 
+
+app_name='blog'
 urlpatterns = [
     path("", views.index, name="index"),  # Ensure this exists
-    path("/detail", views.detail, name="detail"), 
+    path("post/<str:post_id>", views.detail, name="detail"), 
+    path("new_url", views.new_url_view, name="new_url_page"),
+    path("old_url", views.old_url_view, name="old_url"),
 ]
--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.urls import reverse
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello I am rukaa")
 
-def detail(request):
+def detail(request,post_id):
+    return HttpResponse(f'Hello I am rukaa .And the id is {post_id}')
+
+def old_url_view(request):
+    return redirect(reverse('blog:new_url_page'))
+
+def new_url_view(request):
     return HttpResponse("Hello I am rukaa")
+
+
+

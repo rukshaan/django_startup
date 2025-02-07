37f2adc5dfd3a38d09b6ce5488139245077c6263
Rukshan <shaanruk0309@gmail.com>
2025-02-07 13:08:53 +0530

    #8_logger(debugging_msg)

--- myapp/blog/urls.py
+++ myapp/blog/urls.py
@@ -6,7 +6,7 @@ from . import views  # Ensure views is properly imported
 app_name='blog'
 urlpatterns = [
     path("", views.index, name="index"),  # Ensure this exists
-    path("post/<str:post_id>", views.detail, name="detail"), 
+    path("post/<int:post_id>", views.detail, name="detail"), 
     path("new_url", views.new_url_view, name="new_url_page"),
     path("old_url", views.old_url_view, name="old_url"),
 ]

16df453667494643d1c57d9509f16feee096f924
Rukshan <shaanruk0309@gmail.com>
2025-02-07 12:15:40 +0530

    #7_404_page_success

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
-    "/blog/static/blog"
+   "/blog/static/blog"
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
